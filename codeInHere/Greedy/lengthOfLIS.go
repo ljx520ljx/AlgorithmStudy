04844fddb86876acdd75bd9aab73555468b51999
ljx520ljx <3266229323@qq.com>
2025-03-18 16:20:01 +0800

    3-18 update

--- codeInHere/Greedy/lengthOfLIS.go
+++ codeInHere/Greedy/lengthOfLIS.go
@@ -4,7 +4,7 @@ package Greedy
 /*
 算法思路:贪心+二分查找
 核心思想：
-使用一个数组 d，其中 d[i] 表示长度为 i+1i+1i+1 的递增子序列的最小可能末尾元素。
+使用一个数组 d，其中 d[i] 表示长度为 i+1的递增子序列的最小可能末尾元素。
 d 的长度即为当前找到的最长递增子序列的长度。
 贪心策略：
 每次遇到一个新数 num，希望它尽可能小地插入到递增子序列中，以便为后续的数字留出更大的空间。
@@ -22,7 +22,7 @@ d 的长度即为当前找到的最长递增子序列的长度。
 */
 func lengthOfLIS(nums []int) int {
 	// 定义一个数组 d 用于存储当前递增子序列的最小末尾元素
-	d := []int{}
+	var d []int
 
 	for _, num := range nums {
 		// 使用二分查找找到 d 中第一个大于等于 num 的位置
@@ -47,4 +47,35 @@ func lengthOfLIS(nums []int) int {
 
 	// d 的长度即为最长递增子序列的长度
 	return len(d)
+}
+
+//也可以用动态规划
+func lengthOfLIS1(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+
+	// 定义 dp 数组，dp[i] 表示以 nums[i] 结尾的最长上升子序列长度
+	dp := make([]int, n)
+	for i := range dp {
+		dp[i] = 1 // 初始化每个位置的长度至少为 1（自身）
+	}
+
+	// 动态规划计算
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			// 如果 nums[i] > nums[j]，则可以从 dp[j] 转移过来
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+	}
+
+	// 从 dp 中取最大值即为答案
+	res := 0
+	for _, v := range dp {
+		res = max(res, v)
+	}
+	return res
 }
